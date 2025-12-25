ff1a1ef6a30975824b6198ff47b8c6da0eef0d69
Ashish Kumar <helloashish20@gmail.com>
2025-12-26 00:26:41 +0530

    Fix CRA build + clean Projects section

--- src/components/sections/Projects.jsx
+++ src/components/sections/Projects.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React from "react";
 import styled from "styled-components";
 import { projects } from "../../data/constants";
 import ProjectCard from "../cards/ProjectCard";
@@ -6,10 +6,10 @@ import ProjectCard from "../cards/ProjectCard";
 const Container = styled.div`
   display: flex;
   flex-direction: column;
-  justify-contnet: center;
+  justify-content: center;
   margin-top: 50px;
   padding: 0px 16px;
-  position: rlative;
+  position: relative;
   z-index: 1;
   align-items: center;
 `;
@@ -48,40 +48,6 @@ const Desc = styled.div`
   }
 `;
 
-const ToggleButtonGroup = styled.div`
-  display: flex;
-  border: 1.5px solid ${({ theme }) => theme.primary};
-  color: ${({ theme }) => theme.primary};
-  font-size: 16px;
-  border-radius: 12px;
-font-weight 500;
-margin: 22px 0;
-@media (max-width: 768px){
-    font-size: 12px;
-}
-`;
-const ToggleButton = styled.div`
-  padding: 8px 18px;
-  border-radius: 6px;
-  cursor: pointer;
-  &:hover {
-    background: ${({ theme }) => theme.primary + 20};
-  }
-  @media (max-width: 768px) {
-    padding: 6px 8px;
-    border-radius: 4px;
-  }
-  ${({ active, theme }) =>
-    active &&
-    `
-  background:  ${theme.primary + 20};
-  `}
-`;
-const Divider = styled.div`
-  width: 1.5px;
-  background: ${({ theme }) => theme.primary};
-`;
-
 const CardContainer = styled.div`
   display: flex;
   justify-content: center;
@@ -91,7 +57,6 @@ const CardContainer = styled.div`
 `;
 
 const Projects = () => {
-  const [toggle, setToggle] = useState("all");
   return (
     <Container id="Projects">
       <Wrapper>
@@ -101,32 +66,14 @@ const Projects = () => {
             marginBottom: "40px",
           }}
         >
-          I have worked on a wide range of projects.  Here are some of my projects.
+          I have worked on a wide range of projects. Here are some of my
+          projects.
         </Desc>
 
-        {/* <ToggleButtonGroup>
-          
-          <Divider />
-          <ToggleButton
-            active={toggle === "web app"}
-            onClick={() => setToggle("web app")}
-          >
-            WEB APP"S
-          </ToggleButton>
-          <Divider />
-         
-          <Divider />
-          
-        </ToggleButtonGroup> */}
-
         <CardContainer>
-          {toggle === "all" &&
-            projects.map((project) => <ProjectCard project={project} />)}
-          {projects
-            .filter((item) => item.category === toggle)
-            .map((project) => (
-              <ProjectCard project={project} />
-            ))}
+          {projects.map((project) => (
+            <ProjectCard key={project.id} project={project} />
+          ))}
         </CardContainer>
       </Wrapper>
     </Container>
--- src/data/constants.js
+++ src/data/constants.js
@@ -5,7 +5,7 @@ export const Bio = {
     "I am a motivated and versatile individual, always eager to take on new challenges. With a passion for learning I am dedicated to delivering high-quality results. With a positive attitude and a growth mindset, I am ready to make a meaningful contribution and achieve great things.",
   github: "https://github.com/ashish117840",
   resume:
-    "https://drive.google.com/file/d/1eenBQsUF0x2ut1tPd_R8Be7uOYl4ChnS/view?usp=sharing",
+    "https://drive.google.com/file/d/1qeDoqtTulu8SMZEcQX8yCnLukFPIp7TI/view?usp=sharing",
   linkedin: "https://www.linkedin.com/in/ashish-kumar7000/",
   twitter: "https://twitter.com/AshishKumar",
   insta: "https://www.instagram.com/ashish_kumar_300/",
